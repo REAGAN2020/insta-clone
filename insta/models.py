8cb6263ed7eae47646d855e97692e8eddbdcf0ab
REAGAN2020 <reaganteflon@gmail.com>
2020-11-24 21:57:39 +0300

    added models

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
+class Image(models.Model):
+    ''' a model for Image posts '''
+    image = models.ImageField(upload_to='images/')
+    caption = models.TextField()
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
