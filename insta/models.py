ced8190657549152a026c3c478d888172d6a4c90
REAGAN2020 <reaganteflon@gmail.com>
2020-11-24 21:58:50 +0300

    added save image method

--- insta/models.py
+++ insta/models.py
@@ -12,3 +12,26 @@ class Image(models.Model):
     profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['?']  # order randomly on feed
+
+    def save_image(self):
+        ''' method to save an image post instance '''
+        self.save()
+
+    # def delete_image(self):
+    #     '''method to delete an image post instance '''
+    #     self.delete()
+
+    # def update_caption(self, new_caption):
+    #     ''' method to update an image's caption '''
+    #     self.caption = new_caption
+    #     self.save()
+
+    # @classmethod
+    # def get_user_images(cls, user_id):
+    #     ''' method to retrieve all images'''
+    #     img = Image.objects.filter(profile=user_id).all()
+    #     sort = sorted(img, key=lambda t: t.created_on)
+    #     return sort
