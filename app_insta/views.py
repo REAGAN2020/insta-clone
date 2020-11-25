912b84127f100d8ac9d2868ed5d2d5acb7a19b7c
REAGAN2020 <reaganteflon@gmail.com>
2020-11-26 01:35:19 +0300

    inserted view function

--- app_insta/views.py
+++ app_insta/views.py
@@ -19,50 +19,50 @@ def profile(request):
     pics = Image.objects.filter(profile=current_user).all()
     return render(request, 'profile.html', {'pics':pics})
 
-def user(request, user_id):
-    users = User.objects.filter(id=user_id)
-    pics = Image.objects.filter(profile=user_id).all()    
-    return render(request, 'user.html', {'pics':pics, 'users':users}) 
+# def user(request, user_id):
+#     users = User.objects.filter(id=user_id)
+#     pics = Image.objects.filter(profile=user_id).all()    
+#     return render(request, 'user.html', {'pics':pics, 'users':users}) 
 
-@login_required(login_url='/accounts/login/')
-def new_image(request):
-    current_user = request.user.profile
-    if request.method == 'POST':
-        form = uploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            image = form.save(commit=False)
-            image.profile = current_user
-            image.save()
-        return redirect('feed')
-    else:
-        form = uploadForm()
-    return render(request, 'new_image.html', {'form':form})
+# @login_required(login_url='/accounts/login/')
+# def new_image(request):
+#     current_user = request.user.profile
+#     if request.method == 'POST':
+#         form = uploadForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             image = form.save(commit=False)
+#             image.profile = current_user
+#             image.save()
+#         return redirect('feed')
+#     else:
+#         form = uploadForm()
+#     return render(request, 'new_image.html', {'form':form})
 
-@login_required(login_url='accounts/login')
-def comments(request, id):
-    current_user = request.user.profile
-    post = Image.objects.filter(id=id)
+# @login_required(login_url='accounts/login')
+# def comments(request, id):
+#     current_user = request.user.profile
+#     post = Image.objects.filter(id=id)
 
-    if request.method == 'POST':
-        form = commentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.name = current_user
-            comment.related_post = post
-            comment.save()
-        return redirect('comments')
-    else:
-        form = commentForm()
+#     if request.method == 'POST':
+#         form = commentForm(request.POST)
+#         if form.is_valid():
+#             comment = form.save(commit=False)
+#             comment.name = current_user
+#             comment.related_post = post
+#             comment.save()
+#         return redirect('comments')
+#     else:
+#         form = commentForm()
 
-    maoni = Comment.objects.filter(related_post=id).all()
-    return render(request, 'comments.html', {'maoni':maoni, 'form':form})
+#     maoni = Comment.objects.filter(related_post=id).all()
+#     return render(request, 'comments.html', {'maoni':maoni, 'form':form})
 
 
-def like_post(request, post_id):
-    current_user = request.user
-    img = Image.objects.get(id=post_id)
-    if img.likes.filter(id=current_user.id).exists():
-        img.likes.remove(current_user)
-    else:
-        img.likes.add(current_user)
-    return redirect('feed')
+# def like_post(request, post_id):
+#     current_user = request.user
+#     img = Image.objects.get(id=post_id)
+#     if img.likes.filter(id=current_user.id).exists():
+#         img.likes.remove(current_user)
+#     else:
+#         img.likes.add(current_user)
+#     return redirect('feed')
