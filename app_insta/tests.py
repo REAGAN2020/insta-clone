82ecb5fda6acd8f601f79371bc06e20f45fbd499
REAGAN2020 <reaganteflon@gmail.com>
2020-11-26 01:34:28 +0300

    added some tests in image

--- app_insta/tests.py
+++ app_insta/tests.py
@@ -22,25 +22,25 @@ class ProfileTest(TestCase):
         self.assertIsInstance(self.user.profile, Profile)
         
 
-# class TestImage(TestCase):
-#     ''' test class for image model '''
-#     def setUp(self):
-#         ''' method called before each test case'''
-#         self.test_user = User(username='Linda', password='123')
-#         self.test_user.save()
-#         self.test_profile = self.test_user.profile
-#         self.test_profile.save()
+class TestImage(TestCase):
+    ''' test class for image model '''
+    def setUp(self):
+        ''' method called before each test case'''
+        self.test_user = User(username='Linda', password='123')
+        self.test_user.save()
+        self.test_profile = self.test_user.profile
+        self.test_profile.save()
 
-#         self.test_image = Image(image='images/test.jpg', caption='some text', profile=self.test_profile, created_on=datetime.now())
+        self.test_image = Image(image='images/test.jpg', caption='some text', profile=self.test_profile, created_on=datetime.now())
 
-#     def test_instance(self):
-#         ''' test method to ensure image instance creation '''
-#         self.assertTrue(isinstance(self.test_image, Image))
+    def test_instance(self):
+        ''' test method to ensure image instance creation '''
+        self.assertTrue(isinstance(self.test_image, Image))
 
-#     def test_save(self):
-#         ''' test method to save image instance to db '''
-#         self.test_image.save_image()
-#         self.assertEqual(len(Image.objects.all()), 1)
+    def test_save(self):
+        ''' test method to save image instance to db '''
+        self.test_image.save_image()
+        self.assertEqual(len(Image.objects.all()), 1)
 
     def tearDown(self):
         ''' method to clear all setup instances after each test run '''
