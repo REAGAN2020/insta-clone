3d8cde1a89f1130a0db3ae74d7a81f1274b6d9f5
REAGAN2020 <reaganteflon@gmail.com>
2020-11-26 01:32:35 +0300

    class mete

--- app_insta/models.py
+++ app_insta/models.py
@@ -109,8 +109,8 @@ class Relation(models.Model):
     follower = models.ForeignKey('Profile', related_name='following', on_delete=models.CASCADE)
     followed = models.ForeignKey('Profile', related_name='followers', on_delete=models.CASCADE)
 
-#     class Meta:
-#         unique_together = ('follower', 'followed')
+    class Meta:
+        unique_together = ('follower', 'followed')
 
-#     def __str__(self):
-#         return '{self.follower} follows {self.followed}'
+    def __str__(self):
+        return '{self.follower} follows {self.followed}'
