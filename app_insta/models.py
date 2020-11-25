c4fc3f365f46d5715c83dffc970a33c9b74bd2aa
REAGAN2020 <reaganteflon@gmail.com>
2020-11-26 01:30:15 +0300

    added receier and defined it

--- app_insta/models.py
+++ app_insta/models.py
@@ -65,14 +65,14 @@ class Profile(models.Model):
         self.photo = new_image
         self.save()
         
-# @receiver(post_save, sender=User)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
-# @receiver(post_save, sender=User)
-# def save_user_profile(sender, instance, **kwargs):
-#     instance.profile.save()
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 
 
